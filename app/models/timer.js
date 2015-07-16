f6dd6c7380014ec10d3593b20501cfb41687c3ba
Justin Carver <jccarver@gmail.com>
2015-07-16 02:36:35 -0400

    setting up Ember Data to work with the RESTserver I am using on my local machine to run the back end.

---
+++ app/models/timer.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.model.extend({
+    name: DS.attr('string'),
+    roundNo: DS.attr('number', {defaultValue: 1}),
+    duration: DS.attr('number', {defaultValue: 1}),
+    startTime: DS.attr('date',
+        {defaultValue: function () { return new Date(); }}),
+    active: DS.attr('boolean', {defaultValue: false})
+});
