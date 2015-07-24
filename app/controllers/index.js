1d4a70df1c8d399b63cb5a4a7a3dc0073b94b2d0
Justin Carver <jccarver@gmail.com>
2015-07-24 04:54:15 -0400

    Starting working on the styling of the app

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,23 +3,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     actions: {
-        create : function () {
-            var warning = Ember.$('.warning');
-            var roomName = this.get('roomName');
-            warning.html("");
-            if ( undefined !== roomName && roomName.length > 0 ) {
-                var timer = this.store.createRecord('timer', {
-                    name: roomName
-                });
-                var self = this;
-                timer.save().then(
-                    function (timer) {
-                        self.transitionToRoute('manage',timer);
-                    }
-                );
-            } else {
-                warning.html("Please enter a valid name");
-            }
+        create : function (roomName) {
+
+            var timer = this.store.createRecord('timer', {
+                name: roomName
+            });
+            var self = this;
+            timer.save().then(
+                function (timer) {
+                    self.transitionToRoute('manage',timer);
+                }
+            );
         }
     }
 });
